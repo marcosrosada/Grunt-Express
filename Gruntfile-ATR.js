e7e8e8f4788ccffc68d4e4fb7d366ac876d52185
Marcos Rosada <ochild@gmail.com>
2016-04-06 16:31:44 -0300

    First Commit

---
+++ Gruntfile-ATR.js
@@ -0,0 +1,120 @@
+module.exports = function (grunt) {
+
+	var path_atr_webapp = '../requisito/src/main/webapp/dist/atr/',
+		path_atr_webdev = '../requisito/src/main/webdev/atr/';
+
+	grunt.config.merge({
+		clean: {
+			options: { force: true },
+	        atrAll: path_atr_webapp,
+	        atrTemp: [ 
+        			path_atr_webapp + 'js/scripts.js', 
+        			path_atr_webapp + 'js/scripts.min.js', 
+        			path_atr_webapp + 'js/libs.min.js'
+				]
+	    },
+		jshint: {
+        	atrFiles: {
+        		src: path_atr_webdev + 'js/modules/*.js'
+        	}
+	    },
+	    concat: {
+	        atrScripts: {
+            	src: [ 
+						path_atr_webdev + 'js/modules/app.js',
+            		   	path_atr_webdev + 'js/modules/form-input-data.js',						
+						path_atr_webdev + 'js/modules/universe-elements.js',	
+						path_atr_webdev + 'js/modules/project.js',
+						path_atr_webdev + 'js/modules/header.js', 
+						path_atr_webdev + 'js/services/**/*.js',
+						path_atr_webdev + 'js/controllers/**/*.js'
+            		],
+            	dest: path_atr_webapp + 'js/scripts.js'
+	        },
+	        atrLibs: {
+            	src: [ 
+            			path_atr_webdev + 'js/Chart.js',		
+		
+						path_atr_webdev + 'js/jquery/jquery.min.js',
+						path_atr_webdev + 'js/jquery/jquery-ui.min.js',
+						
+						path_atr_webdev + 'js/angular/angular.min.js',						
+						path_atr_webdev + 'js/angular/angular-resource.js',
+						path_atr_webdev + 'js/angular/angular-route.js',
+						path_atr_webdev + 'js/angular/angular-filter.js',
+						path_atr_webdev + 'js/angular/angular-animate.js',
+						path_atr_webdev + 'js/angular/angularjs-dropdown-multiselect.js',
+						path_atr_webdev + 'js/angular/angular-highlightjs.js',
+						path_atr_webdev + 'js/angular/angularjs-dropdown-multiselect-custom.js',
+						path_atr_webdev + 'js/angular/angularjs-dropdown-multiselect-auto-complete.js',
+						path_atr_webdev + 'js/angular/angularjs-dropdown-multiselect-docs.js',		
+						
+						path_atr_webdev + 'js/libs/ngDialog.min.js',
+						path_atr_webdev + 'js/libs/angular-chart.js',
+						path_atr_webdev + 'js/libs/lodash.min.js',
+						path_atr_webdev + 'js/libs/highlight.min.js',
+						path_atr_webdev + 'js/libs/angular-ui-tree.js',
+						path_atr_webdev + 'js/libs/widgets/module.js',
+						path_atr_webdev + 'js/libs/widgets/jqdialog.js',		
+						path_atr_webdev + 'js/libs/angular-tooltips.min.js',
+						path_atr_webdev + 'js/libs/toaster.js',					
+						
+						path_atr_webdev + 'js/bootstrap/bootstrap.min.js',	
+						path_atr_webdev + 'js/bootstrap/ui-bootstrap-tpls-0.9.0.min.js'
+            		],
+            	dest: path_atr_webapp + 'js/libs.min.js'
+	        },
+	        atrAll: {
+	            src: [
+	            		path_atr_webapp + 'js/libs.min.js',
+	            		path_atr_webapp + 'js/scripts.min.js'
+	            	],
+	            dest: path_atr_webapp + 'js/app.min.js'
+	        }
+	    },
+	    uglify: {
+	    	options: {
+		      mangle: false
+		    },
+	    	atrFiles: {
+	            src: [ path_atr_webapp + 'js/scripts.js'],
+	            dest: path_atr_webapp + 'js/scripts.min.js'
+	        }
+	    },
+	    cssmin: {
+	        atrFiles: {
+	            src: [
+	              path_atr_webdev + 'css/**/**/*.css'
+	            ],
+	            dest: path_atr_webapp + 'css/app.min.css'
+	        }
+	    },
+	    htmlmin: {
+	    	atrFiles: {
+	    		options: {
+		          removeComments: true,
+		          collapseWhitespace: true
+		        },
+	            expand: true,
+	            cwd: path_atr_webdev + 'views/',
+	            src: ['**/*.html'],
+	            dest: path_atr_webapp + 'views'
+	    	}
+	    },
+	    copy: {
+	        atrIndex: {
+	            src: path_atr_webdev + 'index-prod.html',
+	            dest: path_atr_webapp + 'index.html'
+	        },
+	        atrImages: {
+	        	expand: true,
+	        	flatten: false,
+	        	cwd: path_atr_webdev + 'image/', 
+	        	src: ['**/*'], 
+	        	dest: path_atr_webapp + 'image/'
+	        }
+	    }
+	});
+
+	grunt.registerTask('atr', ['clean:atrAll', 'jshint:atrFiles', 'concat:atrScripts', 'uglify:atrFiles', 'concat:atrLibs', 'concat:atrAll', 'cssmin:atrFiles', 'htmlmin:atrFiles', 'copy:atrIndex', 'copy:atrImages', 'clean:atrTemp']);
+}
---
+++ Gruntfile-MCDA.js
@@ -0,0 +1,112 @@
+module.exports = function (grunt) {
+
+	var path_mcda_webapp = '../mcda/src/main/webapp/dist/mcda/',
+		path_mcda_webdev = '../mcda/src/main/webdev/mcda/';
+
+	grunt.config.merge({
+		clean: {
+			options: { force: true },
+	        mcdaAll: path_mcda_webapp,
+	        mcdaTemp: [ 
+        			path_mcda_webapp + 'js/scripts.js', 
+        			path_mcda_webapp + 'js/scripts.min.js', 
+        			path_mcda_webapp + 'js/libs.min.js'
+				]
+	    },
+		jshint: {
+        	mcdaFiles: {
+        		src: path_mcda_webdev + 'js/modules/*.js'
+        	}
+	    },
+	    concat: {
+	        mcdaScripts: {
+            	src: [ 
+						path_mcda_webdev + 'js/modules/app.js', 
+						path_mcda_webdev + 'js/services/**/*.js',
+						path_mcda_webdev + 'js/controllers/**/*.js'
+            		],
+            	dest: path_mcda_webapp + 'js/scripts.js'
+	        },
+	        mcdaLibs: {
+            	src: [ 
+            			path_mcda_webdev + 'js/jquery/jquery-1.10.2.min.js',
+						path_mcda_webdev + 'js/jquery/jquery.easing.min.js',
+
+						
+						path_mcda_webdev + 'js/angular/angular.min.js',
+						path_mcda_webdev + 'js/angular/angular-route.js',
+						path_mcda_webdev + 'js/angular/angular-resource.js',
+						path_mcda_webdev + 'js/angular/angular-animate.js',
+						path_mcda_webdev + 'js/angular/angular-highlightjs.js',
+						path_mcda_webdev + 'js/angular/angular-touch.min.js',
+						path_mcda_webdev + 'js/angular/angular-sanitize.js',
+						path_mcda_webdev + 'js/angular/angularjs-dropdown-multiselect.js',
+						
+						
+						path_mcda_webdev + 'js/libs/lodash.min.js',
+						path_mcda_webdev + 'js/libs/highlight.min.js',
+						path_mcda_webdev + 'js/libs/ngDialog.min.js',
+						path_mcda_webdev + 'js/libs/angular-input-masks/masks.js',
+						path_mcda_webdev + 'js/libs/angular-locale_pt-br.js',
+						path_mcda_webdev + 'js/libs/toaster.js',
+						
+						
+						path_mcda_webdev + 'js/bootstrap/ui-bootstrap-tpls.js',
+						path_mcda_webdev + 'js/bootstrap/bootstrap.min.js',
+            		],
+            	dest: path_mcda_webapp + 'js/libs.min.js'
+	        },
+	        mcdaAll: {
+	            src: [
+	            		path_mcda_webapp + 'js/libs.min.js',
+	            		path_mcda_webapp + 'js/scripts.min.js'
+	            	],
+	            dest: path_mcda_webapp + 'js/app.min.js'
+	        }
+	    },
+	    uglify: {
+	    	options: {
+		      mangle: false
+		    },
+	    	mcdaFiles: {
+	            src: [ path_mcda_webapp + 'js/scripts.js'],
+	            dest: path_mcda_webapp + 'js/scripts.min.js'
+	        }
+	    },
+	    cssmin: {
+	        mcdaFiles: {
+	            src: [
+	              path_mcda_webdev + 'css/**/**/*.css'
+	            ],
+	            dest: path_mcda_webapp + 'css/app.min.css'
+	        }
+	    },
+	    htmlmin: {
+	    	mcdaFiles: {
+	    		options: {
+		          removeComments: true,
+		          collapseWhitespace: true
+		        },
+	            expand: true,
+	            cwd: path_mcda_webdev + 'views/',
+	            src: ['**/*.html'],
+	            dest: path_mcda_webapp + 'views'
+	    	}
+	    },
+	    copy: {
+	        mcdaIndex: {
+	            src: path_mcda_webdev + 'index-prod.html',
+	            dest: path_mcda_webapp + 'index.html'
+	        },
+	        mcdaImages: {
+	        	expand: true,
+	        	flatten: false,
+	        	cwd: path_mcda_webdev + 'images/', 
+	        	src: ['**/*'], 
+	        	dest: path_mcda_webapp + 'images/'
+	        }
+	    }
+	});
+
+	grunt.registerTask('mcda', ['clean:mcdaAll', 'jshint:mcdaFiles', 'concat:mcdaScripts', 'uglify:mcdaFiles', 'concat:mcdaLibs', 'concat:mcdaAll', 'cssmin:mcdaFiles', 'htmlmin:mcdaFiles', 'copy:mcdaIndex', 'copy:mcdaImages', 'clean:mcdaTemp']);
+}
