5d6309a43e97aed29c332ffa8c7ee8f23ff62613
Marcos Rosada <ochild@gmail.com>
2016-04-29 16:31:18 -0300

    New features

--- Gruntfile-ATR.js
+++ Gruntfile-ATR.js
@@ -1,7 +1,7 @@
 module.exports = function (grunt) {
 
-	var path_atr_webapp = '../ATR/trunk/src/main/webapp/',
-		path_atr_webdev = '../ATR/trunk/src/main/webdev/atr/';
+	
+	require('./GruntConfig');
 
 	grunt.config.merge({
 		clean: {
@@ -18,54 +18,54 @@ module.exports = function (grunt) {
 		jshint: {
         	atrFiles: {
         		src: [
-        			path_atr_webdev + 'js/modules/**/*.js',
-        			path_atr_webdev + 'js/controllers/**/*.js',
-        			path_atr_webdev + 'js/services/**/*.js'
+        			path_atr_webdev + 'atr/js/modules/**/*.js',
+        			path_atr_webdev + 'atr/js/controllers/**/*.js',
+        			path_atr_webdev + 'atr/js/services/**/*.js'
         		]
         	}
 	    },
 	    concat: {
 	        atrScripts: {
             	src: [ 
-						path_atr_webdev + 'js/modules/app.js',
-            		   	path_atr_webdev + 'js/modules/form-input-data.js',						
-						path_atr_webdev + 'js/modules/universe-elements.js',	
-						path_atr_webdev + 'js/modules/project.js',
-						path_atr_webdev + 'js/modules/header.js', 
-						path_atr_webdev + 'js/services/**/*.js',
-						path_atr_webdev + 'js/controllers/**/*.js'
+						path_atr_webdev + 'atr/js/modules/app.js',
+            		   	path_atr_webdev + 'atr/js/modules/form-input-data.js',						
+						path_atr_webdev + 'atr/js/modules/universe-elements.js',	
+						path_atr_webdev + 'atr/js/modules/project.js',
+						path_atr_webdev + 'atr/js/modules/header.js', 
+						path_atr_webdev + 'atr/js/services/**/*.js',
+						path_atr_webdev + 'atr/js/controllers/**/*.js'
             		],
             	dest: path_atr_webapp + 'js/scripts.js'
 	        },
 	        atrLibs: {
             	src: [ 
-            			path_atr_webdev + 'js/Chart.js',		
-						path_atr_webdev + 'js/jquery/jquery.min.js',
-						path_atr_webdev + 'js/jquery/jquery-ui.min.js',
+            			path_atr_webdev + 'atr/js/Chart.js',		
+						path_atr_webdev + 'atr/js/jquery/jquery.min.js',
+						path_atr_webdev + 'atr/js/jquery/jquery-ui.min.js',
 						
-						path_atr_webdev + 'js/angular/angular.min.js',						
-						path_atr_webdev + 'js/angular/angular-resource.js',
-						path_atr_webdev + 'js/angular/angular-route.js',
-						path_atr_webdev + 'js/angular/angular-filter.js',
-						path_atr_webdev + 'js/angular/angular-animate.js',
-						path_atr_webdev + 'js/angular/angularjs-dropdown-multiselect.js',
-						path_atr_webdev + 'js/angular/angular-highlightjs.js',
-						path_atr_webdev + 'js/angular/angularjs-dropdown-multiselect-custom.js',
-						path_atr_webdev + 'js/angular/angularjs-dropdown-multiselect-auto-complete.js',
-						path_atr_webdev + 'js/angular/angularjs-dropdown-multiselect-docs.js',		
+						path_atr_webdev + 'atr/js/angular/angular.min.js',						
+						path_atr_webdev + 'atr/js/angular/angular-resource.js',
+						path_atr_webdev + 'atr/js/angular/angular-route.js',
+						path_atr_webdev + 'atr/js/angular/angular-filter.js',
+						path_atr_webdev + 'atr/js/angular/angular-animate.js',
+						path_atr_webdev + 'atr/js/angular/angularjs-dropdown-multiselect.js',
+						path_atr_webdev + 'atr/js/angular/angular-highlightjs.js',
+						path_atr_webdev + 'atr/js/angular/angularjs-dropdown-multiselect-custom.js',
+						path_atr_webdev + 'atr/js/angular/angularjs-dropdown-multiselect-auto-complete.js',
+						path_atr_webdev + 'atr/js/angular/angularjs-dropdown-multiselect-docs.js',		
 						
-						path_atr_webdev + 'js/libs/ngDialog.min.js',
-						path_atr_webdev + 'js/libs/angular-chart.js',
-						path_atr_webdev + 'js/libs/lodash.min.js',
-						path_atr_webdev + 'js/libs/highlight.min.js',
-						path_atr_webdev + 'js/libs/angular-ui-tree.js',
-						path_atr_webdev + 'js/libs/widgets/module.js',
-						path_atr_webdev + 'js/libs/widgets/jqdialog.js',		
-						path_atr_webdev + 'js/libs/angular-tooltips.min.js',
-						path_atr_webdev + 'js/libs/toaster.js',					
+						path_atr_webdev + 'atr/js/libs/ngDialog.min.js',
+						path_atr_webdev + 'atr/js/libs/angular-chart.js',
+						path_atr_webdev + 'atr/js/libs/lodash.min.js',
+						path_atr_webdev + 'atr/js/libs/highlight.min.js',
+						path_atr_webdev + 'atr/js/libs/angular-ui-tree.js',
+						path_atr_webdev + 'atr/js/libs/widgets/module.js',
+						path_atr_webdev + 'atr/js/libs/widgets/jqdialog.js',		
+						path_atr_webdev + 'atr/js/libs/angular-tooltips.min.js',
+						path_atr_webdev + 'atr/js/libs/toaster.js',					
 						
-						path_atr_webdev + 'js/bootstrap/bootstrap.min.js',	
-						path_atr_webdev + 'js/bootstrap/ui-bootstrap-tpls-0.9.0.min.js'
+						path_atr_webdev + 'atr/js/bootstrap/bootstrap.min.js',	
+						path_atr_webdev + 'atr/js/bootstrap/ui-bootstrap-tpls-0.9.0.min.js'
             		],
             	dest: path_atr_webapp + 'js/libs.min.js'
 	        },
@@ -94,7 +94,7 @@ module.exports = function (grunt) {
 		    	dest: path_atr_webapp + 'js/scripts.port.js',
 		    	replacements: [{
 		      		from: ':8080',
-		      		to: ':8080'
+		      		to: ''
 		    	}]
 		  	},
 		  	atrPathFonts: {
@@ -121,7 +121,7 @@ module.exports = function (grunt) {
 	    cssmin: {
 	        atrFiles: {
 	            src: [
-	              path_atr_webdev + 'css/**/**/*.css'
+	              path_atr_webdev + 'atr/css/**/**/*.css'
 	            ],
 	            dest: path_atr_webapp + 'css/app.css'
 	        }
@@ -133,32 +133,38 @@ module.exports = function (grunt) {
 		          collapseWhitespace: true
 		        },
 	            expand: true,
-	            cwd: path_atr_webdev + 'views/',
+	            cwd: path_atr_webdev + 'atr/views/',
 	            src: ['**/*.html'],
 	            dest: path_atr_webapp + 'views'
 	    	}
 	    },
-	    copy: {
+	    copy: {			
 	        atrIndex: {
-	            src: path_atr_webdev + 'index-prod.html',
+	            src: path_atr_webdev + 'atr/index-prod.html',
 	            dest: path_atr_webapp + 'index.html'
+	        },
+			atrSecure: {
+				expand: true,
+	        	cwd: path_atr_webdev + 'atr/secure/', 
+	        	src: ['**/*.html'],
+	            dest: path_atr_webapp + 'secure'
 	        },
 	        atrImages: {
 	        	expand: true,
 	        	flatten: false,
-	        	cwd: path_atr_webdev + 'image/', 
+	        	cwd: path_atr_webdev + 'atr/image/', 
 	        	src: ['**/*'], 
 	        	dest: path_atr_webapp + 'image/'
 	        },
 	        atrFonts: {
 	        	expand: true,
 	        	flatten: false,
-	        	cwd: path_atr_webdev + 'css/fonts/', 
+	        	cwd: path_atr_webdev + 'atr/css/fonts/', 
 	        	src: ['**/*'], 
 	        	dest: path_atr_webapp + 'css/fonts/'
 	        }
 	    }
 	});
 
-	grunt.registerTask('atr', ['clean:atrAll', 'jshint:atrFiles', 'concat:atrScripts', 'removeLoggingCalls:atrFiles', 'replace:atrPort', 'uglify:atrFiles', 'concat:atrLibs', 'concat:atrAll', 'cssmin:atrFiles', 'replace:atrPathFonts', 'htmlmin:atrFiles', 'copy:atrIndex', 'copy:atrImages', 'copy:atrFonts','clean:atrTemp']);
+	grunt.registerTask('atr', ['clean:atrAll', 'jshint:atrFiles', 'concat:atrScripts', 'removeLoggingCalls:atrFiles', 'replace:atrPort', 'uglify:atrFiles', 'concat:atrLibs', 'concat:atrAll', 'cssmin:atrFiles', 'replace:atrPathFonts', 'htmlmin:atrFiles', 'copy:atrIndex', 'copy:atrSecure', 'copy:atrImages', 'copy:atrFonts','clean:atrTemp']);
 }
